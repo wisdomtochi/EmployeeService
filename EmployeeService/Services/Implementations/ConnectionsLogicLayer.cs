12b00e84b02ff26d5d32f36d438f6f64d93d8870
Wisdom Tochi <agbasionyewisdom@gmail.com>
2023-10-21 02:09:36 -0700

    EmployeeService

--- EmployeeService/Services/Implementations/ConnectionsLogicLayer.cs
+++ EmployeeService/Services/Implementations/ConnectionsLogicLayer.cs
@@ -1,7 +1,7 @@
 ﻿using EmployeeService.Data;
 using EmployeeService.Domains;
 using EmployeeService.Services.Interfaces;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections;
 
 namespace EmployeeService.Services.Implementations
@@ -15,24 +15,32 @@ namespace EmployeeService.Services.Implementations
             this.context = context;
         }
 
-        public async Task<string> AddToConnection(Employee employeeModel)
+        public async Task<string> AddToConnection(int employeeId, int connectionId)
         {
-            var emp = await context.Employees.FindAsync(employeeModel.Id);
+            Employee employee = await context.Employees.FirstOrDefaultAsync(x => x.Id == employeeId);
+            Employee connection = await context.Employees.FirstOrDefaultAsync(x => x.Id == connectionId);
 
-            //await context.Employees.;
-
-            if (emp == null)
+            if (employee != null && connection != null)
             {
-                return $"{employeeModel.FirstName} {employeeModel.LastName} could not not be found in the database";
-            };
 
-            return "You have added a new connection";
+
+                if (!employee.Connections.Contains(connection))
+                {
+                    employee.Connections.Add(connection);
+                    await context.SaveChangesAsync();
+                }
+                else
+                {
+                    return "Already in your connections";
+                }
+            };
+            return " The employee or Customer could not be found in the database";
         }
 
-        public async Task<List<Connection>> GetConnectionList([FromRoute] int Id, [FromBody] Connection connectModel)
+        public async Task<List<Connection>> GetConnectionList(int Id)
         {
-            await context.Connections.FindAsync(Id);
-            return (List<Connection>)(IEnumerable)connectModel.Employees;
+            Connection connectEmployee = await context.Connections.FirstOrDefaultAsync(x => x.Id == Id);
+            return (List<Connection>)(IEnumerable)connectEmployee.Employees;
         }
 
         public IEnumerable<Connection> ConnectionList()
--- EmployeeService/Services/Interfaces/IConnectionsLogicLayer.cs
+++ EmployeeService/Services/Interfaces/IConnectionsLogicLayer.cs
@@ -4,10 +4,10 @@ namespace EmployeeService.Services.Interfaces
 {
     public interface IConnectionsLogicLayer
     {
-        Task<string> AddToConnection(Employee employeeModel);
+        Task<string> AddToConnection(int employeeId, int connectionId);
         IEnumerable<Connection> ConnectionList();
         //IEnumerable<Employee> ConnectionsRequestList();
         //void SendConnectionRequest(Employee emp, ConnectEmployeeViewModel connectEmployee);
-        Task<List<Connection>> GetConnectionList(int Id, Connection connectModel);
+        Task<List<Connection>> GetConnectionList(int Id);
     }
 }
