e635af108a7d3bb2a5b8545b7901822a5f18f65c
Wisdom Tochi <agbasionyewisdom@gmail.com>
2023-10-24 02:19:25 -0700

    EmployeeService

--- EmployeeService/Data/Migrations/20231024091706_AddingConnectionRequestTable.Designer.cs
+++ EmployeeService/Data/Migrations/20231024091706_AddingConnectionRequestTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EmployeeService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmployeeService.Migrations
 {
     [DbContext(typeof(EmployeeDbContext))]
-    partial class EmployeeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231024091706_AddingConnectionRequestTable")]
+    partial class AddingConnectionRequestTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EmployeeService/Domains/ConnectionRequest.cs
+++ EmployeeService/Domains/ConnectionRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace EmployeeService.Domains
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeService.Domains
 {
     public class ConnectionRequest
     {
+        [Key]
         public int ReceiverId { get; set; }
         public int SenderId { get; set; }
         public string? RequestNotification { get; set; }
