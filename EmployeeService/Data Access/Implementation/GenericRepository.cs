8e1fbfc17a35e123fa443702f6be424dd23e3608
Wisdom Tochi <agbasionyewisdom@gmail.com>
2023-10-27 01:14:23 -0700

    EmployeeService

---
+++ EmployeeService/Data Access/Implementation/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using EmployeeService.Data;
+using EmployeeService.Data_Access.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeService.Data_Access.Implementation
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly EmployeeDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(EmployeeDbContext context)
+        {
+            _dbContext = context;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public async Task<T> ReadSingle(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> ReadAll()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public void Update(T entity)
+        {
+            var updateEntity = _dbSet.Attach(entity);
+            updateEntity.State = EntityState.Modified;
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<bool> SaveChanges()
+        {
+            return await _dbContext.SaveChangesAsync() >= 0;
+        }
+    }
+}
---
+++ EmployeeService/Data Access/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeService.Data_Access.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> ReadSingle(int id);
+        Task<IEnumerable<T>> ReadAll();
+        Task<T> Create(T entity);
+        Task Delete(int id);
+        void Update(T entity);
+        Task<bool> SaveChanges();
+    }
+}
