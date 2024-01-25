7df907f9825adde8c7865637f84ce83cc6539fe4
Wisdom Tochi <agbasionyewisdom@gmail.com>
2024-01-25 14:36:02 -0800

    New Changes

---
+++ EmployeeService/Data/Migrations/20240125032209_UpdatingMigration.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeService.Migrations
+{
+    public partial class UpdatingMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "LastName",
+                keyValue: null,
+                column: "LastName",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Gender",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "FirstName",
+                keyValue: null,
+                column: "FirstName",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ConnectionRequests",
+                columns: table => new
+                {
+                    ReceiverId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    SenderId = table.Column<int>(type: "int", nullable: false),
+                    RequestNotification = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConnectionRequests", x => x.ReceiverId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Connections",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connections", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ConnectionEmployee",
+                columns: table => new
+                {
+                    ConnectionsId = table.Column<int>(type: "int", nullable: false),
+                    EmployeesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConnectionEmployee", x => new { x.ConnectionsId, x.EmployeesId });
+                    table.ForeignKey(
+                        name: "FK_ConnectionEmployee_Connections_ConnectionsId",
+                        column: x => x.ConnectionsId,
+                        principalTable: "Connections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConnectionEmployee_Employees_EmployeesId",
+                        column: x => x.EmployeesId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            //migrationBuilder.CreateIndex(
+            //    name: "IX_Employees_Id",
+            //    table: "Employees",
+            //    column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConnectionEmployee_EmployeesId",
+                table: "ConnectionEmployee",
+                column: "EmployeesId");
+
+            //migrationBuilder.AddForeignKey(
+            //    name: "FK_Employees_Id",
+            //    table: "Employees",
+            //    column: "Id",
+            //    principalTable: "Employees",
+            //    principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Employees_EmployeeId",
+                table: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "ConnectionEmployee");
+
+            migrationBuilder.DropTable(
+                name: "ConnectionRequests");
+
+            migrationBuilder.DropTable(
+                name: "Connections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_EmployeeId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
